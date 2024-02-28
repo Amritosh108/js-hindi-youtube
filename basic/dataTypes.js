c77ecff3206514e170e10f2e287b43d5a65414a1
Amritosh kumar pandey <141411181+Amritosh108@users.noreply.github.com>
2024-02-28 16:09:29 +0000

    dataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,33 @@
+console.log("Amriotsh");
+console.log(typeof "Amriosh");
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.BigInt
+// 5.Null
+// 6.Undefinded
+// 7.Object
+// 8.Symbol
+
+// Number
+let x = 15
+let y = 15
+console.log(typeof x)
+
+// String
+let  Name = "Amriosh pandey"
+console.log(typeof Name);
+
+//Boolean
+let present = true
+
+console.log(typeof present);
+
+//Null
+
+let state = null;
+console.log(typeof state);
+
+//Undefined
+let city = undefined
+console.log(typeof city); 
